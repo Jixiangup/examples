09e5887a32ad2062c2f848e13061d260a88dd340
jixiangup <jixiangup@gmail.com>
2024-04-12 11:23:33 +0800

    Refactor: Unified http status source(#1)

--- golang/crud/gin/internal/controller/index.go
+++ golang/crud/gin/internal/controller/index.go
@@ -1,10 +1,13 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func Error(context *gin.Context, exception error) {
 	if exception == nil {
-		context.JSON(500, gin.H{"message": "Internal Server Error", "status": 500})
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Internal Server Error", "status": 500})
 	}
 	_ = context.Error(exception)
 	context.Abort()
